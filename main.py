328efb248f7bdfabafd205781e733c71bacb43a4
josevictormoreno <josevictor.admoreno@gmail.com>
2022-05-30 16:41:09 -0300

    feat: first upload test

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from pydrive.auth import GoogleAuth
 import os
 
 gauth = GoogleAuth()
+print('Linha 1')
 gauth.LocalWebserverAuth()
+print('Linha 2')
 drive = GoogleDrive(gauth)
+print('linha 3')
+path = r"Drive"
+print('Linha 4')
+print('antes do for')
 
-
-path = "caminho"
-
-for x in os.listdir(path:
+for x in os.listdir(path):
     f = drive.CreateFile({'title': x})
-f.SetContentFile(os.path.join(path, x))
-f.Upload()
-
-f= None
+    f.SetContentFile(os.path.join(path, x))
+    f.Upload()
+    f = None
